2db79b4d1db824b95174c16df5103e4a788d3807
Dan Bushong <bushongdan@gmail.com>
2021-01-27 17:46:06 -0600

    engineer added

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
-// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor (name, id, email){
+        this.name = name;
+        this.id= id;
+        this.email=email;
+    }
+    retrieveName(){
+        return this.name;
+
+    }
+    retrieveID(){
+        return this.id;
+    }
+    retrieveEmail(){
+        return this.email;
+    }
+    retrieveJob(){
+        return "Employee";
+
+    }
+}
+module.exports = Employee;
