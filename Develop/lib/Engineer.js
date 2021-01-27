5165bb653a9949aed596a72e23ac23db57dc18c6
Dan Bushong <bushongdan@gmail.com>
2021-01-27 17:53:55 -0600

    intern updated

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -5,7 +5,7 @@ class Engineer extends Employee {
         super(name, id, email);
         this.github = github
     }
-    getJob(){
+    getRole(){
         return "Engineer"
     }
     getGithub(){
